2265710a723a35d4301855d8460e1b6877511d3d
linzangsc <105103686+linzangsc@users.noreply.github.com>
2024-09-24 21:07:08 +0800

    Update model.py

--- model.py
+++ model.py
@@ -8,35 +8,37 @@ class EnergyBasedModel(nn.Module):
         self.batch_size = batch_size
         self.input_channel = input_channel
         self.net = nn.Sequential(
-            nn.Linear(input_channel, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, 512),
+            nn.Linear(input_channel, 512),
             nn.ReLU(),
             nn.Linear(512, 256),
             nn.ReLU(),
             nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Linear(128, 1),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 8),
+            nn.ReLU(),
+            nn.Linear(8, 1),
         )
     
-    def sample_by_langevin(self, init_x, max_step=10, epsilon=1e-3):
+    def sample_by_langevin(self, init_x, max_step=100, epsilon=10):
         x = init_x
+        # x = torch.randn_like(init_x)
         x.requires_grad = True
-        noise = torch.randn_like(x)
         for i in range(max_step):
-            energy = self.calculate_energy(x).sum()
-            grad = torch.autograd.grad(outputs=energy, inputs=x, create_graph=True)[0]
+            noise = torch.randn_like(x)/255.
+            energy = self.net(x).sum()
+            grad = torch.autograd.grad(outputs=energy, inputs=x)[0]
+            # if grad.max() > 10: print(f"grad: {grad.max()}")
             x = x + epsilon*grad + torch.sqrt(torch.tensor(2.*epsilon))*noise
+            x = x.clip(0., 1.)
         return x
-
-    def calculate_energy(self, x):
-        return torch.exp(self.net(x).squeeze())
 
     def forward(self, x):
         # shape of x: (bs, c, h, w)
         batch_size, input_channel, height, weight = x.shape
         x = x.reshape(batch_size, -1)
-        energy_real = self.calculate_energy(x)
-        energy_model = self.calculate_energy(self.sample_by_langevin(x))
+        energy_real = self.net(x).squeeze()
+        energy_langevin = self.net(self.sample_by_langevin(x)).squeeze()
 
-        return energy_real, energy_model
+        return energy_real, energy_langevin
