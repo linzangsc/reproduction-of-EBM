b8cb48b4be7b02632e2d7cab89cca28d83678736
linzangsc <105103686+linzangsc@users.noreply.github.com>
2024-09-25 16:39:22 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,39 @@
 import torch
 import torch.nn as nn
+from utils import Sampler
+
+class Swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+class CNNModel(nn.Module):
+
+    def __init__(self, hidden_features=32, out_dim=1, **kwargs):
+        super().__init__()
+        # We increase the hidden dimension over layers. Here pre-calculated for simplicity.
+        c_hid1 = hidden_features//2
+        c_hid2 = hidden_features
+        c_hid3 = hidden_features*2
+        
+        # Series of convolutions and Swish activation functions
+        self.cnn_layers = nn.Sequential(
+                nn.Conv2d(1, c_hid1, kernel_size=5, stride=2, padding=4), # [16x16] - Larger padding to get 32x32 image
+                Swish(),
+                nn.Conv2d(c_hid1, c_hid2, kernel_size=3, stride=2, padding=1), #  [8x8]
+                Swish(),
+                nn.Conv2d(c_hid2, c_hid3, kernel_size=3, stride=2, padding=1), # [4x4]
+                Swish(),
+                nn.Conv2d(c_hid3, c_hid3, kernel_size=3, stride=2, padding=1), # [2x2]
+                Swish(),
+                nn.Flatten(),
+                nn.Linear(c_hid3*4, c_hid3),
+                Swish(),
+                nn.Linear(c_hid3, out_dim)
+        )
+
+    def forward(self, x):
+        x = self.cnn_layers(x).squeeze(dim=-1)
+        return x
 
 class EnergyBasedModel(nn.Module):
     def __init__(self, input_channel, batch_size, device):
@@ -7,38 +41,39 @@ class EnergyBasedModel(nn.Module):
         self.device = device
         self.batch_size = batch_size
         self.input_channel = input_channel
-        self.net = nn.Sequential(
-            nn.Linear(input_channel, 512),
-            nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, 8),
-            nn.ReLU(),
-            nn.Linear(8, 1),
-        )
+        # self.net = nn.Sequential(
+        #     nn.Linear(input_channel, 256),
+        #     Swish(),
+        #     nn.Linear(256, 128),
+        #     Swish(),
+        #     nn.Linear(128, 64),
+        #     Swish(),
+        #     nn.Linear(64, 32),
+        #     Swish(),
+        #     nn.Linear(32, 1),
+        # )
+        self.net = CNNModel()
     
     def sample_by_langevin(self, init_x, max_step=100, epsilon=10):
         x = init_x
         # x = torch.randn_like(init_x)
         x.requires_grad = True
+        noise = torch.randn_like(x)
         for i in range(max_step):
-            noise = torch.randn_like(x)/255.
+            noise.normal_(0, 0.005)
             energy = self.net(x).sum()
             grad = torch.autograd.grad(outputs=energy, inputs=x)[0]
+            grad.clamp_(-0.03, 0.03)
             # if grad.max() > 10: print(f"grad: {grad.max()}")
             x = x + epsilon*grad + torch.sqrt(torch.tensor(2.*epsilon))*noise
-            x = x.clip(0., 1.)
-        return x
+            x = x.clip(-1., 1.)
+        return x.detach()
 
     def forward(self, x):
         # shape of x: (bs, c, h, w)
         batch_size, input_channel, height, weight = x.shape
-        x = x.reshape(batch_size, -1)
+        # x = x.reshape(batch_size, -1)
         energy_real = self.net(x).squeeze()
-        energy_langevin = self.net(self.sample_by_langevin(x)).squeeze()
+        energy_langevin = self.net(self.sample_by_langevin(x.detach())).squeeze()
 
         return energy_real, energy_langevin
